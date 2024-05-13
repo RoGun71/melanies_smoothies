fa8d2bfdfacb11f1863ae0da165ff2f5e261bd8d
Ronald Gunther <rongunther@gmail.com>
2024-05-13 10:04:33 -0400

    Update streamlit_app.py
    do api call for each chosen fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,11 @@ ingredients_list = st.multiselect(
     , max_selections = 5
 )
 
+#new section to display fruityvice information
+#fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+#fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
@@ -39,6 +44,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = rq.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -54,8 +61,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
 
-#new section to display fruityvice information
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
