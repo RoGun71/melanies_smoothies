2e2f06442a0558922e7e30c36b369917924d1b96
Ronald Gunther <rongunther@gmail.com>
2024-05-13 09:59:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,4 +56,6 @@ if ingredients_list:
 
 #new section to display fruityvice information
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
